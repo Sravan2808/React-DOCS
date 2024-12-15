8a9646e473e1fd771bb75e3af81582ee559071c2
Sravan Surya <vsravansurya@gmail.com>
2024-12-15 23:36:29 +0530

    code added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export const content = [
+  "./src/**/*.{html,js,jsx,ts,tsx}", // Update this to your specific file paths
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
